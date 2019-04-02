efb1150c5bff3299e99427302093605aeab4ce3f
hortasha <eidemartin_303@hotmail.com>
2019-04-02 11:11:38 +0200

    fix url bug

--- magic/server.js
+++ magic/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname)));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running...'));
