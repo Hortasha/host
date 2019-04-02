63e7fd16ba0cdf9ae4efe2450610141c2a4bd70f
hortasha <eidemartin_303@hotmail.com>
2019-04-02 12:00:37 +0200

    change sub url issue

--- magic/server.js
+++ magic/server.js
@@ -6,9 +6,9 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use(express.static(__dirname));
+app.use('/magic', express.static(__dirname));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/magic/*', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running...'));
