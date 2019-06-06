2d90d24b6f279cd6ef0ae10015e8ebaeaed17676
Martin Eide <eidemartin_303@hotmail.com>
2019-06-06 12:28:46 +0000

    host config

--- holidaze/src/mongodb/connect.js
+++ holidaze/src/mongodb/connect.js
@@ -4,7 +4,7 @@ mongoose.set('useFindAndModify', false);
 
 //Desktop location: C:\MongoDB\Server\4.0
 //Desktop dev env
-const server = 'localhost';
+const server = '127.0.0.1';
 const database = 'holidaze';
 //const user = 'martin';
 //const password = 'Kalle123';
